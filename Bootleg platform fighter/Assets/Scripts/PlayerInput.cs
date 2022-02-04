f0ada24833a9d03de47ce1d523a3662d4b3a66ad
HolyAcorn <74989722+HolyAcorn@users.noreply.github.com>
2022-02-04 11:25:41 +0100

    Basic input system
    Implemented a basic system for differenciating inputs with playerIndex

--- Bootleg platform fighter/Assets/Scripts/PlayerInput.cs
+++ Bootleg platform fighter/Assets/Scripts/PlayerInput.cs
@@ -11,22 +11,25 @@ namespace BootlegPlatformFighter
 
         private BootlegCharacterController characterController;
 
+        private int playerIndex;
+
 
         private void Start()
         {
             characterController = GetComponent<BootlegCharacterController>();
+            playerIndex = characterController.playerIndex;
         }
 
         void FixedUpdate()
         {
-            controls.horizontalInput = Input.GetAxisRaw("Horizontal");
-            controls.verticalInput = Input.GetAxisRaw("Vertical");
+            controls.horizontalInput = Input.GetAxisRaw("Horizontal_" + playerIndex);
+            controls.verticalInput = Input.GetAxisRaw("Vertical_" + playerIndex);
 
-            controls.jumpButton = Input.GetButton("Jump");
+            controls.jumpButton = Input.GetButton("Jump_" + playerIndex);
 
-            controls.airdashButton = Input.GetButton("Airdash");
+            controls.airdashButton = Input.GetButton("Airdash_" + playerIndex);
             // For controllers
-            controls.airdashAxis = Input.GetAxisRaw("Airdash");
+            controls.airdashAxis = Input.GetAxisRaw("Airdash_" + playerIndex);
             characterController.ProcessUpdate(controls);
         }
     }
