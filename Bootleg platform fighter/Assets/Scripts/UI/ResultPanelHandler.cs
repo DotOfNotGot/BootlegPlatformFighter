031be244ba0ebaaeef245d629be02ec4e44939de
Akandesh <Akandessh@live.com>
2022-03-18 14:19:23 +0100

    minor card tweak

--- Bootleg platform fighter/Assets/Scripts/UI/ResultPanelHandler.cs
+++ Bootleg platform fighter/Assets/Scripts/UI/ResultPanelHandler.cs
@@ -7,15 +7,52 @@ namespace BootlegPlatformFighter
 {
     public class ResultPanelHandler : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        }
+        [SerializeField]
+        private bool _open;
+        [SerializeField]
+        private float _animationTime = 0.5f;
+        [SerializeField]
+        private float minHeight = 287f;
+        [SerializeField]
+        private float maxHeight = 535f;
 
+
+        public bool IsOpen => _open;
         public void OnClick()
         {
+            if (!_open)
+            {
+                MaximizeCard();
+                // Loops through all existing cards and minimize the ones that are open, except for this.
+                var cards = transform.parent.GetComponentsInChildren<ResultPanelHandler>();
+                for (int i = 0; i < cards.Length; i++)
+                {
+                    var card = cards[i];
+                    if (card == this)
+                        continue;
+                    if (card.IsOpen) {
+                        card.MinimizeCard();
+                    }
+                }
+            }
+            else
+            {
+                MinimizeCard();
+            }
+        }
+
+        public void MinimizeCard()
+        {
             var defX = gameObject.GetComponent<RectTransform>().sizeDelta.x;
-            gameObject.GetComponent<RectTransform>().DOSizeDelta(new Vector2(defX, 535f), 3f);
+            gameObject.GetComponent<RectTransform>().DOSizeDelta(new Vector2(defX, minHeight), _animationTime);
+            _open = false;
+        }
+
+        public void MaximizeCard()
+        {
+            var defX = gameObject.GetComponent<RectTransform>().sizeDelta.x;
+            gameObject.GetComponent<RectTransform>().DOSizeDelta(new Vector2(defX, maxHeight), _animationTime);
+            _open = true;
         }
     }
 }
