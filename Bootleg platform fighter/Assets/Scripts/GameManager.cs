640b577cd85f6b005941127c103d05a746082ac1
Akandesh <Akandessh@live.com>
2022-03-22 18:20:58 +0100

    fixed deathzones

--- Bootleg platform fighter/Assets/Scripts/GameManager.cs
+++ Bootleg platform fighter/Assets/Scripts/GameManager.cs
@@ -18,33 +18,33 @@ namespace BootlegPlatformFighter
         void GenerateCollidersAcrossScreen()
         {
             // Tweak the corners to adjust when they should be considered <outside>
-            Vector2 lDCorner = Camera.main.ViewportToWorldPoint(new Vector3(-1, -1f, Camera.main.nearClipPlane));
-            Vector2 rUCorner = Camera.main.ViewportToWorldPoint(new Vector3(2f, 2f, Camera.main.nearClipPlane));
+            Vector2 rTCorner = new Vector2(500, 150);
+            Vector2 lBCorner = new Vector2(-500, -50);
             Vector2[] colliderpoints;
 
             EdgeCollider2D upperEdge = new GameObject("upperDeathZone").AddComponent<EdgeCollider2D>();
             colliderpoints = upperEdge.points;
-            colliderpoints[0] = new Vector2(lDCorner.x, rUCorner.y);
-            colliderpoints[1] = new Vector2(rUCorner.x, rUCorner.y);
+            colliderpoints[0] = new Vector2(lBCorner.x, rTCorner.y);
+            colliderpoints[1] = new Vector2(rTCorner.x, rTCorner.y);
             upperEdge.points = colliderpoints;
 
             EdgeCollider2D lowerEdge = new GameObject("lowerDeathZone").AddComponent<EdgeCollider2D>();
             colliderpoints = lowerEdge.points;
-            colliderpoints[0] = new Vector2(lDCorner.x, lDCorner.y);
-            colliderpoints[1] = new Vector2(rUCorner.x, lDCorner.y);
+            colliderpoints[0] = new Vector2(lBCorner.x, lBCorner.y);
+            colliderpoints[1] = new Vector2(rTCorner.x, lBCorner.y);
             lowerEdge.points = colliderpoints;
 
-            EdgeCollider2D leftEdge = new GameObject("leftDeathZone").AddComponent<EdgeCollider2D>();
+            EdgeCollider2D leftEdge = new GameObject("rightDeathZone").AddComponent<EdgeCollider2D>();
             colliderpoints = leftEdge.points;
-            colliderpoints[0] = new Vector2(lDCorner.x, lDCorner.y);
-            colliderpoints[1] = new Vector2(lDCorner.x, rUCorner.y);
+            colliderpoints[0] = new Vector2(rTCorner.x, rTCorner.y);
+            colliderpoints[1] = new Vector2(rTCorner.x, lBCorner.y);
             leftEdge.points = colliderpoints;
 
-            EdgeCollider2D rightEdge = new GameObject("rightDeathZone").AddComponent<EdgeCollider2D>();
+            EdgeCollider2D rightEdge = new GameObject("leftDeathZone").AddComponent<EdgeCollider2D>();
 
             colliderpoints = rightEdge.points;
-            colliderpoints[0] = new Vector2(rUCorner.x, rUCorner.y);
-            colliderpoints[1] = new Vector2(rUCorner.x, lDCorner.y);
+            colliderpoints[0] = new Vector2(lBCorner.x, rTCorner.y);
+            colliderpoints[1] = new Vector2(lBCorner.x, lBCorner.y);
             rightEdge.points = colliderpoints;
         }
     }
